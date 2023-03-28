0c000eb611bbb77f3cd70b613107742af211b003
alessandrosdias <116725639+alessandrosdias@users.noreply.github.com>
2023-03-28 10:30:25 -0300

    Update acessoriosController.js

--- src/controllers/acessoriosController.js
+++ src/controllers/acessoriosController.js
@@ -4,16 +4,25 @@ import acessoriosDAO from "../DAO/acessoriosDAO.js";
 
     const acessoriosController = (app) => {
     app.get("/acessorios", async (req, res) => {
-      const resposta = await clientesDAO.mostrarTodos();
+      const resposta = await acessoriosDAO.mostrarTodos();
       res.status(resposta.status).send(resposta.dados);
     });
-    app.get("/acessorios/email/:email", async (req, res) => {
-      const resposta = await clientesDAO.mostrarUm(req.params.email);
+    app.get("/acessorios/id/:id", async (req, res) => {
+      const resposta = await acessoriosDAO.mostrarUm(req.params.id);
       res.status(resposta.status).send(resposta.dados);
     });
     app.post("/acessorios", async (req, res) => {
-      const modelado = clientes.modelar(req.body)
-      const resposta = await clientesDAO.inserir(modelado)
+      const modelado = acessoriosModel.modelar(req.body)
+      const resposta = await acessoriosDAO.inserir(modelado)
+      res.status(resposta.status).send(resposta.dados);
+    });
+
+    app.delete("/acessorios/id/:id", async(req, res) => {
+      const resposta = await acessoriosDAO.deletar(req.params.id,req.body)
+      res.status(resposta.status).send(resposta.dados)
+    });
+    app.put("/acessorios/id/:id", async (req, res) => {
+      const resposta = await acessoriosDAO.atualizar(req.params.id, req.body);
       res.status(resposta.status).send(resposta.dados);
     });
   };
